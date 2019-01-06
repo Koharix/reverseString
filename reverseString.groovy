2a50e47d3914264a810056141278693f3d843071
Koharix <36640892+Koharix@users.noreply.github.com>
2019-01-06 12:48:07 -0600

    Update reverseString.groovy

--- reverseString.groovy
+++ reverseString.groovy
@@ -7,7 +7,7 @@
  Hope you don't mind I wrote it in groovy.
 
  The other link is to a python service I wrote that checks the PUBG api for updates and stores said updates into a google sheets
- spreadsheet. This is an example of the level of api experience I have when dealing with http calls. The project is currently being
+ spreadsheet. This is an example of the kind of api experience I have when dealing with http calls. The project is currently being
  refactored. I am trying out the pep8 coding standard for python and I am working through that process now. 
 */
 
