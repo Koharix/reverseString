0776a1a707e4bd50d61622f2ee223f2a7e5d69e0
Koharix <36640892+Koharix@users.noreply.github.com>
2019-01-06 14:21:42 -0600

    Update reverseString.groovy

--- reverseString.groovy
+++ reverseString.groovy
@@ -1,5 +1,5 @@
 /*
- I wanted to get back to you with an appropriate solution to the code sample. I believe in implementing built practical solutions 
+ I wanted to get back to you with a better solution to the code sample. I believe in implementing built practical solutions 
  like helper functions, libraries, frameworks, etc to rapidly progress with stable, lightweight, and human-readable code.
 
  Spent ~10 minutes writing then optimizing this script.
