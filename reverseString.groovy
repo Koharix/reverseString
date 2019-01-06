29cb3455788f1debe5f3fdbef89fe29ff66eb966
Koharix <36640892+Koharix@users.noreply.github.com>
2019-01-06 12:39:58 -0600

    Update reverseString.groovy

--- reverseString.groovy
+++ reverseString.groovy
@@ -1,17 +1,17 @@
 /*
- I wanted to get back to you with an appropriate solution to the test code you gave to me. I beleive in practical solutions like 
- using already avalibe soultions like helper functions, libraries, frameworks, etc to produce lightweight and human-readable code.
+ I wanted to get back to you with an appropriate solution to the code test you gave to me. I believe in already built practical 
+ solutions like helper functions, libraries, frameworks, etc to produce lightweight and human-readable code.
  
- Spent ~10 minutes writing and optomizing this script.
+ Spent ~10 minutes writing then optomizing this script.
 
  Hope you don't mind I wrote it in groovy.
 
  The other link is to a python service I wrote that checks the PUBG api for updates and stores said updates into a google sheets
- document. This is an example of the kind of api work I have when dealing with http calls. The project is currently being
+ document. This is an example of the level of api experience I have when dealing with http calls. The project is currently being
  refactored. I am trying out the pep8 coding standard for python and I am working through that process now. 
 */
 
-def str = "A TECH PROJECT AT HR BLOCK"
+def str = " A TECH COMPANY IS THE FUTURE"
 
 println "Initial String:"
 println str
